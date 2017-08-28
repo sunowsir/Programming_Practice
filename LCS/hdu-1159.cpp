25fa646d4a7860933678020b94809770e33263cf
sunowsir <sunowsir@qq.com>
2017-08-28 13:18:35 +0800

    规范化

--- LCS/hdu-1159.cpp
+++ LCS/hdu-1159.cpp
@@ -11,26 +11,28 @@
 using namespace std;
 
 const int maxn = 1001;
-char s[maxn],t[maxn];
+char s[maxn], t[maxn];
 int dp[maxn][maxn];
 
-void lcs(){
-	int n=strlen(s);
-	int m=strlen(t);
+void lcs() {
+	int n = strlen(s);
+	int m = strlen(t);
 	memset(dp,0,sizeof(dp));
-	for(int i=1;i<=n;i++){
-		for(int j=1;j<=m;j++){
-			if(s[i-1] == t[j-1])
-				dp[i][j] = dp[i-1][j-1]+1;
-			else
-				dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			if (s[i - 1] == t[j - 1]){
+				dp[i][j] = dp[i - 1][j - 1] + 1;
+			}
+			else {
+				dp[i][j] = max(dp[i - 1][j],dp[i][j - 1]);
+			}
 		}
 	}
-	printf("%d\n",dp[n][m]);
+	printf("%d\n", dp[n][m]);
 }
 
-int main(){
-	while(cin>>s>>t){
+int main() {
+	while (cin>>s>>t) {
 		lcs();
 	}
 	return 0;
