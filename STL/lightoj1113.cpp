28179ce0aa14b45a9cc8aedacfffb8e614f55d0b
sunowsir <sunowsir@qq.com>
2018-03-13 17:38:15 +0800

    update 2018.3.13

--- STL/lightoj1113.cpp
+++ STL/lightoj1113.cpp
@@ -1,60 +1,69 @@
 /*************************************************************************
-    > File Name: lightoj1113.cpp
-    > Author: sunowsir
-    > Mail: *** ***
-    > Created Time: 2017年07月07日 星期五 13时44分27秒
+	> File Name: lightoj1113.cpp
+	> Author: sunowsir
+	> GitHub: github.com/sunowsir/practice
+	> Created Time: 2018年03月11日 星期日 14时21分35秒
  ************************************************************************/
 
-#include<bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <stack>
 using namespace std;
 
-stack<string> sb,sf;
-queue<string> ss;
+int main() {
 
-int main(){
-	int n,N;
-	string s,web;
-	cin>>n;
-	N=1;
-	while(n--){
-		//把首页入栈；
-		sb.push("http://www.lightoj.com/");
-		cout<<"Case "<<N++<<":"<<endl;
-		while(1){
-			cin>>s;
-			if(s=="VISIT"){
-				cin>>web;
-				sb.push(web);
-				while(!sf.empty())  sf.pop();//根据题意，当back空了后再插入后，之前的网址也回不去了；
-				cout<<web<<endl;
-			}
-			if(s=="BACK"){
-				if(!sb.empty()){
-					sf.push(sb.top());
-					sb.pop();
-					//保证首页是题目所给不被pop掉；
-					if(sb.empty()){
-						sb.push(sf.top());
-						sf.pop();
-						cout<<"Ignored"<<endl;
-					}
-					else  cout<<sb.top()<<endl;
-				}
-			}
-			if(s=="FORWARD"){
-				if(!sf.empty()){
-					sb.push(sf.top());
-					sf.pop();
-					cout<<sb.top()<<endl;
-				}
-				else  cout<<"Ignored"<<endl;
-			}
-			ss.push(sb.top());
-			if(s=="QUIT")  break;
-		}
-		//把两个栈清空；
-		while(!sb.empty())  sb.pop();
-		while(!sf.empty())  sf.pop();
-	}
-	return 0;
+    stack<string> backword, forward;
+    string url, order;
+    int n, N;
+    N = 1;
+
+    cin >> n;
+
+    while(n--) {
+        printf("Case %d:\n", N++);
+        backword.push("http://www.lightoj.com/");
+        while(cin >> order) {
+            if(order[0] == 'Q') {
+                break;
+            }
+            else if(order[0] == 'B') {
+                forward.push(backword.top());
+                backword.pop();
+                if(backword.empty()) {
+                    backword.push(forward.top());
+                    forward.pop();
+                    cout << "Ignored" << endl;
+                }
+                else {
+                    cout << backword.top() << endl;
+                }
+            }
+            else if(order[0] == 'F') {
+                if(!forward.empty()) {
+                    backword.push(forward.top());
+                    forward.pop();
+                    cout << backword.top() << endl;
+                }
+                else {
+                    cout << "Ignored" << endl;
+                }
+            }
+            else if(order[0] == 'V') {
+                while(!forward.empty()) {
+                    forward.pop();
+                }
+                cin >> url;
+                cout << url << endl;
+                backword.push(url);
+            }
+        }
+        while(!backword.empty()) {
+            backword.pop();
+        }
+        while(!forward.empty()) {
+            forward.pop();
+        }
+    }
+
+    return 0;
 }
--- 搜索/poj1979.cpp
+++ 搜索/poj1979.cpp
@@ -1,59 +1,57 @@
 /*************************************************************************
-    > File Name: poj1979.cpp
-    > Author: sunowsir
-    > Mail: *** ***
-    > Created Time: 2017年07月08日 星期六 16时04分20秒
+	> File Name: poj1979.cpp
+	> Author: sunowsir
+	> GitHub: github.com/sunowsir/practice
+	> Created Time: 2018年03月12日 星期一 18时15分46秒
  ************************************************************************/
 
-#include<cstdio>
-#include<iostream>
-#include<cstring>
+#include <iostream>
+#include <cstring>
 using namespace std;
 
-#define cls(x,y) memset(x,y,sizeof(x))
-const int maxn = 100;
-char mp[maxn][maxn];
-int nx[4]={0,0,-1,1};
-int ny[4]={1,-1,0,0};
-int n,m,ans;
-bool book[maxn][maxn];
+int dx[] = {0, 1, 0, -1};
+int dy[] = {1, 0, -1, 0};
+char Map[25][25];
+bool book[25][25];
+int x, y;
 
-int dfs(int x,int y){
-	//每调用一次dfs函数都记一下个数，调用几次就是成功搜索几次。
-	ans++;
-	//此次调用时传入的x和y作为此次搜索的中心点，此次搜索的方向都是以这个x和y为标志物，是该点的上下左右。
-	//把该点标记，表示已经搜索过。
-	book[x][y] = true;
-	for(int i=0;i<4;i++){
-		int dx,dy;
-		//x和y分别赋值上nx[i]和ny[i]dx和dy就变成了x和y的上下左右方向。
-		dx=x+nx[i];
-		dy=y+ny[i];
-		//1.判断该方向的点（此时的dx和dy）是不是还在地图中
-		//2.判断该方向上的点是不是'.'（不懂去看题目要干啥）
-		//3.判断该方向上的点搜没搜索过（用book[]来标记看第26行）
-		if(dx>=0 && dx<m && dy>=0 && dy<n && mp[dx][dy]=='.' && !book[dx][dy])
-			//如果这些条件都符合，把该方向上的点作为中心点，调用dfs。
-			dfs(dx,dy);
-	}
-	return ans;
+void traverse(int sx, int sy, int *num) {
+    book[sx][sy] = true;
+    //判断是否越界；
+    if(sx < 0 || sx >= x || sy < 0 || sy >= y) {
+        return;
+    }
+    for(int i = 0; i < 4; i++) {
+        int nx = sx + dx[i];
+        int ny = sy + dy[i];
+        if(!book[nx][ny] && nx >= 0 && nx < x && ny >= 0 && ny < y && Map[nx][ny] != '#') {
+            (*num)++;
+            traverse(nx, ny, num);
+        }
+    }
+    return;
+
 }
 
-int main(){
-	int x,y;
-	while(scanf("%d%d",&n,&m) && n && m){
-		cls(book,false);
-		cls(mp,0);
-		ans=0;
-		for(int i=0;i<m;i++)
-			for(int j=0;j<n;j++){
-				cin>>mp[i][j];
-				if(mp[i][j] == '@'){
-					x=i;
-					y=j;
-				}
-			}
-		cout<<dfs(x,y)<<endl;
-	}
-	return 0;
+int main() {
+
+    int start_x, start_y, num;
+
+    while(cin >> y >> x && x != 0 && y != 0) {
+        num = 1;
+        memset(book, false, sizeof(book));
+        for(int i = 0; i < x; i++) {
+            for(int j = 0; j < y; j++) {
+                cin >> Map[i][j];
+                if(Map[i][j] == '@') {
+                    start_x = i;
+                    start_y = j;
+                }
+            }
+        }
+        traverse(start_x, start_y, &num);
+        cout << num << endl;
+    }
+
+    return 0;
 }
